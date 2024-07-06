244c0a5ef8ed378622607fe3c091b5ae1c59c610
JZITNIK-github <email@jzitnik.dev>
2024-07-06 23:08:38 +0200

    feat: Added harpoon to config

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -8,3 +8,6 @@ vim.api.nvim_set_keymap('n', 'mp', ':MarkdownPreview<CR>', { noremap = true, sil
 vim.api.nvim_set_keymap('n', 'ms', ':MarkdownPreviewStop<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gl', ':Neogit<CR>', { noremap = true, silent = true })
 
+
+
+
