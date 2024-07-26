3c5b5509c12176aa53504f7339209de20b474e48
JZITNIK-github <email@jzitnik.dev>
2024-07-26 14:20:47 +0200

    feat: Added rustfmt to my nvim config

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -1,19 +1,29 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
 local sources = {
-   formatting.prettier.with({
-      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte", "css", "html", "json", "markdown" }
-   }),
-   formatting.stylua,
+	formatting.prettier.with({
+		filetypes = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"svelte",
+			"css",
+			"html",
+			"json",
+			"markdown",
+		},
+	}),
+	formatting.stylua,
+	formatting.rustfmt,
 
-   lint.shellcheck,
+	lint.shellcheck,
 }
 
-null_ls.setup {
-   debug = true,
-   sources = sources,
-}
-
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
