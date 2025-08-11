759690f5f971c1a8ef448bc08f81d66e2800637b
jzitnik-dev <email@jzitnik.dev>
2025-08-11 15:31:59 +0200

    fix: Hyprland config

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -7,7 +7,7 @@ vim.api.nvim_set_keymap("n", "<leader>mp", ":MarkdownPreview<CR>", { noremap = t
 vim.api.nvim_set_keymap("n", "<leader>ms", ":MarkdownPreviewStop<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>gl", ":Neogit<CR>", { noremap = true, silent = true })
 
-vim.opt.spell = true
+vim.opt.spell = false
 vim.opt.spelllang = { "en_us", "cs" }
 
 vim.api.nvim_create_autocmd("FileType", {
